df96704401ab19e6382bb9075b3d705686220a5c
Michael Redig <mredig@gmail.com>
2019-07-15 11:04:17 -0500

    initial commit

---
+++ Rainblow/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Rainblow
+//
+//  Created by Michael Redig on 7/15/19.
+//  Copyright © 2019 Red_Egg Productions. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
